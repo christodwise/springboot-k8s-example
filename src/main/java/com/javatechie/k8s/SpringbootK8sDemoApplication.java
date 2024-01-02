200d6f1f685a9da7fb0799db5ab394637504aa13
christodwise <151842136+christodwise@users.noreply.github.com>
2024-01-02 19:13:32 +0530

    Update SpringbootK8sDemoApplication.java

--- src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
+++ src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringbootK8sDemoApplication {
 
 	@GetMapping("/message")
 	public String displayMessage(){
-		return "Congratulation you successfully deployed your application to kubernetes 1.0 !!";
+		return "Congratulation you successfully deployed your application to kubernetes 1.1 !!";
 	}
 
 	public static void main(String[] args) {
